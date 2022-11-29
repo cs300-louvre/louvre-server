3e4cf494ab81c448df538b23fc33992a3ee37305
trminhnam <trminhnam20082002@gmail.com>
2022-11-30 00:42:03 +0700

    refactor: add export token function when login/register

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -15,12 +15,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({
-    success: true,
-    token,
-  });
+  sendTokenInResponse(user, 201, res);
 });
 
 // @desc    Login a user
@@ -36,19 +31,29 @@ exports.login = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ email }).select("+password");
 
   if (!user) {
-    return next(new ErrorResponse("Invalid credentials", 401));
+    return next(new ErrorResponse("Email is not registered", 401));
   }
 
   const isMatch = await user.comparePassword(password);
 
   if (!isMatch) {
-    return next(new ErrorResponse("Invalid credentials", 401));
+    return next(new ErrorResponse("Wrong password.", 401));
   }
 
+  sendTokenInResponse(user, 200, res);
+});
+
+const sendTokenInResponse = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({
+  const option = {
+    expires: Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
+    httpOnly: true, // Cookie can't be accessed or modified in any way by the browser
+    secure: process.env.NODE_ENV === "production", // Cookie will only be sent on an encrypted connection
+  };
+
+  res.status(statusCode).cookie("token", token, option).json({
     success: true,
     token,
   });
-});
+};
