19e8f9fc6b509e48fd07f2bc2f726379b18e0931
trminhnam <trminhnam20082002@gmail.com>
2022-12-01 15:06:53 +0700

    feat: add verification the JSON web token with Bearer authentication

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -12,7 +12,7 @@ const errorHandler = (err, req, res, next) => {
 
   // Mongoose duplicate key
   if (err.code === 11000) {
-    const message = "Duplicate field value entered";
+    const message = "Email has been used";
     error = new ErrorResponse(message, 400);
   }
 
@@ -20,6 +20,12 @@ const errorHandler = (err, req, res, next) => {
   if (err.name === "ValidationError") {
     const message = Object.values(err.errors).map((val) => val.message);
     error = new ErrorResponse(message, 400);
+  }
+
+  // Wrong JWT token
+  if (err.name === "JsonWebTokenError") {
+    const message = "Invalid token";
+    error = new ErrorResponse(message, 401);
   }
 
   res.status(error.statusCode || 500).json({
