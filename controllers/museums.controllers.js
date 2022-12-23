a837909d0591579289ea6807ab8a33f3a8c26828
trminhnam <trminhnam20082002@gmail.com>
2022-12-24 00:56:58 +0700

    feat: get all museums (normal retrieval without filters)

--- controllers/museums.controllers.js
+++ controllers/museums.controllers.js
@@ -3,6 +3,19 @@ const Museum = require("../models/Museum");
 const asyncHandler = require("../middlewares/asyncHandler");
 const ErrorResponse = require("../utils/errorResponse");
 
+// @desc    Get all museums
+// @route   GET /api/museums
+// @access  Public
+exports.getMuseums = asyncHandler(async (req, res, next) => {
+  const museums = await Museum.find();
+
+  res.status(200).json({
+    success: true,
+    count: museums.length,
+    data: museums,
+  });
+});
+
 // @desc    Create new museum
 // @route   POST /api/museums
 // @access  Private
--- routes/api/museums/museums.routes.js
+++ routes/api/museums/museums.routes.js
@@ -5,6 +5,8 @@ const auth = require("../../../middlewares/auth");
 
 const router = express.Router();
 
+router.route("/").get(museumAPI.getMuseums);
+
 router.route("/").post(auth.verifyToken, museumAPI.createMuseum);
 
 module.exports = router;
